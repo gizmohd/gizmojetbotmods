8a36276fcb2ea0bc6d81e62cdb354e732536018f
Darren McDaniel <gizmohd@gmail.com>
2019-04-04 17:32:40 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .camera import Camera
 from .heartbeat import Heartbeat
-from robot import Robot
+from .robot import Robot
 from .image import bgr8_to_jpeg
 from .object_detection import ObjectDetector
