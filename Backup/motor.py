32528040e99d070b2d771086bf601e301b071eec
Darren McDaniel <gizmohd@gmail.com>
2019-04-13 17:51:36 -0400

    Updated GitIgnore

---
+++ Backup/motor.py
@@ -0,0 +1,48 @@
+import atexit
+from adafruit_motorkit import MotorKit
+import traitlets
+from traitlets.config.configurable import Configurable
+
+
+class Motor(Configurable):
+
+    value = traitlets.Float()
+    
+    # config
+    alpha = traitlets.Float(default_value=1.0).tag(config=True)
+    beta = traitlets.Float(default_value=0.0).tag(config=True)
+
+    def __init__(self, driver, channel, *args, **kwargs):
+        super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
+
+        self._driver = driver
+        if (channel == 1):
+            self._motor = self._driver.motor1
+        else:
+            self._motor = self._driver.motor2
+        atexit.register(self._release)
+
+    def set_min_max(self, speed):
+        if (speed == 0):
+           return speed
+        elif (speed > 1):
+           return 1
+        elif (speed < -1):
+           return -1
+        elif (speed > 0):
+           return  0.3 + (speed * .7)
+        else:
+           return  -0.3 - (speed * .7)
+
+
+    @traitlets.observe('value')
+    def _observe_value(self, change):
+        self._write_value(change['new'])
+
+    def _write_value(self, value):
+        """Sets motor value between [-1, 1]"""
+        self._motor.throttle = self.set_min_max(value)
+#        self._motor.throttle = value
+
+    def _release(self):
+        self._motor.throttle = 0
---
+++ Backup/robot.py
@@ -0,0 +1,53 @@
+from time import sleep
+import traitlets
+from traitlets.config.configurable import SingletonConfigurable
+from adafruit_motorkit import MotorKit
+from .motor import Motor
+#from .PCA9685 import PCA9685
+
+sleep_time = 0.05
+
+class Robot(SingletonConfigurable):
+    
+    left_motor = traitlets.Instance(Motor) 
+    right_motor = traitlets.Instance(Motor)
+    # config
+    left_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
+    left_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
+    right_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
+    right_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Robot, self).__init__(*args, **kwargs)
+        self.motor_driver = MotorKit(0x40)
+#        self.servo_driver = PCA9685(0x70)
+#        self.servo_driver.setPWMFreq(50)
+        self.left_motor = Motor(self.motor_driver, channel=self.left_motor_channel, alpha=self.left_motor_alpha)
+        self.right_motor = Motor(self.motor_driver, channel=self.right_motor_channel, alpha=self.right_motor_alpha)
+
+    def set_servo(self, id, position):
+        self.servo_driver.setServoPulse(id, position)
+
+    def set_motors(self, left_speed, right_speed):
+        self.left_motor.value = left_speed
+        self.right_motor.value = right_speed
+
+    def forward(self, speed=1.0, duration=None):
+        self.left_motor.value = speed
+        self.right_motor.value = speed
+
+    def backward(self, speed=1.0):
+        self.left_motor.value = -speed
+        self.right_motor.value = -speed
+
+    def left(self, speed=1.0):
+        self.left_motor.value = -speed
+        self.right_motor.value = speed
+
+    def right(self, speed=1.0):
+        self.left_motor.value = speed
+        self.right_motor.value = -speed
+
+    def stop(self):
+        self.left_motor.value = 0
+        self.right_motor.value = 0
